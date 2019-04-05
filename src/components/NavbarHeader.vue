803dc128afe6f5b50d6a325113604f14ed91f509
姜港生 <jianggangsheng@192.168.0.100>
2019-04-05 18:59:45 +0800

    1

---
+++ src/components/NavbarHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header">
+        <ul>
+            <li>我的</li>
+            <li>发现</li>
+            <li>朋友</li>
+            
+            <li>视频</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'NavbarHeader'
+}
+</script>
+
+<style>
+
+</style>
