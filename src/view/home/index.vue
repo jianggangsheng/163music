b9be6289e89efa1f4a32baf4821a3c78b26de4e9
姜港生 <jianggangsheng@192.168.0.100>
2019-04-24 00:46:24 +0800

    轮播

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <slide ref="slide" :autoPlay="isAutoPlay" :loop="isLoop" :showDot="isShowDot" :interval="interval" :threshold="threshold" :speed="speed">
-        <div v-for="(item,index) in data" :key="index">
+        <div v-for="(item,index) in items" :key="index">
             <a :href="item.linkUrl" target="_blank">
                 <img :src="item.picUrl">
             </a>
@@ -109,7 +109,7 @@
 </template>
 
 <script>
-var items = [
+// var items = [
     //   {
     //     linkUrl: 'http://y.qq.com/w/album.html?albummid=0044K2vN1sT5mE',
     //     picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000001YCZlY3aBifi.jpg',
@@ -136,7 +136,7 @@ var items = [
     //     id: 11287
     //   }
     
-  ]
+//   ]
 import slide from '@/components/Slide.vue'
 export default {
     name:'index',
@@ -146,11 +146,11 @@ export default {
     components: {
       slide
     },
-    computed: {
-      data() {
-        return items
-      }
-    },
+    // computed: {
+    //   items() {
+    //     // return items
+    //   }
+    // },
     watch: {
       index() {
         this.$refs.slide.update()
@@ -158,6 +158,34 @@ export default {
     },
     data(){
       return{
+          items:[
+      {
+        linkUrl: 'http://y.qq.com/w/album.html?albummid=0044K2vN1sT5mE',
+        picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000001YCZlY3aBifi.jpg',
+        id: 11351
+      },
+      {
+        linkUrl: 'https://y.qq.com/m/digitalbum/gold/index.html?_video=true&id=2197820&g_f=shoujijiaodian',
+        picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000004ckGfg3zaho0.jpg',
+        id: 11372
+      },
+      {
+        linkUrl: 'http://y.qq.com/w/album.html?albummid=001tftZs2RX1Qz',
+        picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M00000236sfA406cmk.jpg',
+        id: 11378
+      },
+      {
+        linkUrl: 'https://y.qq.com/msa/218/0_4085.html',
+        picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000001s0BXx3Zxcwb.jpg',
+        id: 11375
+      },
+      {
+        linkUrl: 'https://y.qq.com/m/digitalbum/gold/index.html?_video=true&id=2195876&g_f=shoujijiaodian',
+        picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000002cwng4353HKz.jpg',
+        id: 11287
+      }
+    
+  ],
         scroll:'',
         index: 0,
         turnToPrev: false,
@@ -180,14 +208,16 @@ export default {
         _this.$api.post('banner',{},(res)=>{
           if(res.code == 200){
             res.banners.map(function(item,index,input){
-              _banner.push({
-                'linkUrl':item.url,
-                'picUrl':item.imageUrl,
-                'id':item.encodeId
-              })
+                _banner.push({
+                    'linkUrl':item.url,
+                    'picUrl':item.imageUrl,
+                    'id':item.encodeId,
+                })
             })
-            items=_banner
-            _this.scroll.refresh()
+            console.log(_banner)
+            console.log(_this.items)
+            console.log('1')
+            // _this.items=res.banners.splice(4)
           }else{
           }
           
